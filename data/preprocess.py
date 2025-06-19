0ce49b0a5cdb67d8868ace3f3d265cbf60afa5ea
Wangang Cheng <167403967+WangangCheng@users.noreply.github.com>
2025-06-20 01:14:25 +0800

    Update preprocess.py

--- data/preprocess.py
+++ data/preprocess.py
@@ -8,21 +8,21 @@ import nibabel as nib
 
 # train
 train_set = {
-        'root': '/home/setdata/ubuntu2204/cwg/t-CURLora/Datasets/EADC-LPBA40',
+        'root': 'Your_Path/t-CURLora/Datasets/EADC-LPBA40',
         'flist': 'train.txt',
         'has_label': True
         }
 
 # test/validation data
 valid_set = {
-        'root': '/home/setdata/ubuntu2204/cwg/t-CURLora/Datasets/EADC-LPBA40',
+        'root': 'Your_Path/t-CURLora/Datasets/EADC-LPBA40',
         'flist': 'valid.txt',
         'has_label': True
         }
 
 
 test_set = {
-        'root': '/home/setdata/ubuntu2204/cwg/t-CURLora/Datasets/EADC-LPBA40',
+        'root': 'Your_Path/t-CURLora/Datasets/EADC-LPBA40',
         'flist': 'test.txt',
         'has_label': False
         }
